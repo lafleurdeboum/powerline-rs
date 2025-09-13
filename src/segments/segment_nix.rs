62f3fc6313f7b49bac679d4e2895a5978af96dd3
lafleur <lafleur@boum.org>
2025-09-13 14:33:55 +0200

    nix segment: replace "impure" with a star for flakes

--- src/segments/segment_nix.rs
+++ src/segments/segment_nix.rs
@@ -4,10 +4,17 @@ use crate::{Powerline, Segment};
 pub fn segment_nix(p: &mut Powerline) {
     // TODO: Generalize this to any environment variable?
     if let Ok(val) = env::var("IN_NIX_SHELL") {
-        p.segments.push(Segment::new(
-            p.theme.nixshell_bg,
-            p.theme.nixshell_fg,
-            val
-        ));
+        match val.as_str() {
+            "impure" => p.segments.push(Segment::new(
+                p.theme.nixshell_bg,
+                p.theme.nixshell_fg,
+                "*"
+            )),
+            _ => p.segments.push(Segment::new(
+                p.theme.nixshell_bg,
+                p.theme.nixshell_fg,
+                val
+            )),
+        }
     }
 }
